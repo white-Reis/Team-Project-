c0a51394944460f920c7abc4d2cb7d2c2635c9bf
Fabio Reis <82837278+white-Reis@users.noreply.github.com>
2022-08-12 06:58:58 -0300

    commit correct social

--- APP/JS/social.js
+++ APP/JS/social.js
@@ -4,14 +4,14 @@ function testeValidSocial() {
   let expressaoUrl = new RegExp('^(https?|chrome)://[^s$.?#].[^s]*$')
   let correct = 0
 
-  if (!expressaoLinkedin.test($('#inputLinkedin').value)) {
-    let spanLinkedin = document.getElementById('spanLinkedin')
-    spanLinkedin.classList.remove('invisible')
-  } else {
-    correct++
-    let spanLinkedin = document.getElementById('spanLinkedin')
-    spanLinkedin.classList.add('invisible')
-  }
+  // if (!expressaoLinkedin.test($('#inputLinkedin').value)) {
+  //   let spanLinkedin = document.getElementById('spanLinkedin')
+  //   spanLinkedin.classList.remove('invisible')
+  // } else {
+  //   correct++
+  //   let spanLinkedin = document.getElementById('spanLinkedin')
+  //   spanLinkedin.classList.add('invisible')
+  // }
   if (!expressaoGitHub.test($('#inputGitHub').value)) {
     let spanGitHub = document.getElementById('spanGitHub')
     spanGitHub.classList.remove('invisible')
@@ -21,8 +21,8 @@ function testeValidSocial() {
     spanGitHub.classList.add('invisible')
   }
 
-  setInterval((correct == 2 ? submitSocial.classList.remove('btn-danger') : submitSocial.classList.add('btn-danger')),1000)
-  correct == 2 ? (flagPage = 3) : (flagPage = 2)
-  correct == 2 ? basicCorrect() : console.log('ha ' + (correct - 2 + ' erros'))
+  setInterval((correct == 1 ? submitSocial.classList.remove('btn-danger') : submitSocial.classList.add('btn-danger')),1000)
+  correct == 1 ? (flagPage = 3) : (flagPage = 2)
+  correct == 1 ? basicCorrect() : {}
   pagination()
 }
